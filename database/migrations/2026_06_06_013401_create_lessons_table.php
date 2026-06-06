4d1f165ce3616ebaf3ce8d0505e7c050ecb04899
oatabaresmoreno-crypto <oa,tabaresmoreno@gmail.com>
2026-06-05 20:52:39 -0500

    Primer commit

---
+++ database/migrations/2026_06_06_013401_create_lessons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up(): void
+{
+    Schema::create('courses', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('instructor_id')->constrained('users')->restrictOnDelete();
+        $table->string('title');
+        $table->string('slug')->unique();
+        $table->text('description')->nullable();
+        $table->string('cover_image')->nullable();
+        $table->enum('level', ['beginner', 'intermediate', 'advanced']);
+        $table->boolean('active')->default(true);
+        $table->timestamps();
+    });
+}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
